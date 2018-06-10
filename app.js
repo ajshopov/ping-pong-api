d763f511c90dfd6ee540bc99d84cb87339e3f874
ajshopov <alexanderjshopov@gmail.com>
2018-06-11 09:19:21 +1000

    database setup

--- app.js
+++ app.js
@@ -2,21 +2,12 @@ const express = require('express');
 const app = express();
 const PORT = 5000;
 
-// const initOptions = {
-//     // pg-promise initialization options...
-//     connect(client, dc, isFresh) {
-//         const cp = client.connectionParameters;
-//         console.log('Connected to database:', cp.database);
-//     }
-// };
 const pgp = require('pg-promise')();
-const cn = 'postgres://username:password@host:port/database';
 const connection = {
     database: 'ping_pong',
     user: 'ajshopov'
 };
 const db = pgp(connection);
-
 
 
 
@@ -29,11 +20,7 @@ app.get('/', function(req, res) {
 
 
 app.get('/api/players', (req, res) => {
-  // db.any('SELECT * FROM users')
-  //   .then(function(data) {
-  //       console.log(data);
-  //   })
-  db.any('select * from players')
+  db.any('SELECT * FROM players')
     .then(data => {
       console.log(data); // print data;
       res.json(data)
@@ -41,10 +28,6 @@ app.get('/api/players', (req, res) => {
       console.log('ERROR:', error); // print the error;
     })
 })
-
-
-
-
 
 
 app.listen(PORT, function() {
