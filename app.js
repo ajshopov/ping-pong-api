1c135660a0a9b4705fbf1dfed8598ad09d0d504c
jwon114 <jthgwong@gmail.com>
2018-06-11 14:10:52 +1000

    cors and body-parser

--- app.js
+++ app.js
@@ -5,11 +5,9 @@ const PORT = 5000;
 const pgp = require('pg-promise')();
 const connection = {
     database: 'ping_pong',
-    user: 'ajshopov'
+    user: 'James'
 };
 const db = pgp(connection);
-
-
 
 app.get('/', function(req, res) {
   const test = {
@@ -17,9 +15,6 @@ app.get('/', function(req, res) {
   }
   res.json(test)
 });
-
-<<<<<<< HEAD
-=======
 
 app.get('/api/players', (req, res) => {
   db.any('SELECT * FROM players')
@@ -32,7 +27,6 @@ app.get('/api/players', (req, res) => {
 })
 
 
->>>>>>> d763f511c90dfd6ee540bc99d84cb87339e3f874
 app.listen(PORT, function() {
 	console.log(`listening on port ${PORT}`);
 });
